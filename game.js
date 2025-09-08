4b322381274919b7c7ea493988baa6530bbe987e
iakaktus <139704105+iakaktus@users.noreply.github.com>
2025-09-08 16:13:47 +0300

    Update game.js

--- game.js
+++ game.js
@@ -30,7 +30,7 @@ window.addEventListener('DOMContentLoaded', () => {
   let score = 0;
   let lives = 3;
   let level = 1;
-  let gameSpeed = 5; // Увеличили скорость (меньше = быстрее)
+  let gameSpeed = 1; // Увеличили скорость (меньше = быстрее)
   let gameRunning = false;
   let lastTime = 0;
   let accumulator = 0;
@@ -910,3 +910,4 @@ window.addEventListener('DOMContentLoaded', () => {
   initGame();
 
 });
+
